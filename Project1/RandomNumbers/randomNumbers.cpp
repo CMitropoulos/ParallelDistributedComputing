d3e9e14a956e0f38846673137a4f299f20455913
root <root@node1-1.sb8.orbit-lab.org>
2017-09-29 21:47:00 -0400

    update random number generator to write 20mil random floats to file

--- Project1/RandomNumbers/randomNumbers.cpp
+++ Project1/RandomNumbers/randomNumbers.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <random>
+#include <fstream> 
+
+using namespace std;
+
 int main()
 {
-    std::random_device rd; // obtain a random number from hardware
-    std::mt19937 eng(rd()); // seed the generator
-    std::uniform_real_distribution<> distr(0,8); // define the range
+    random_device rd; // obtain a random number from hardware
+    mt19937 eng(rd()); // seed the generator
+    uniform_real_distribution<> distr(0,8); // define the range
+   
+    ofstream writeStream; 
+    writeStream.open("randomNumbers.txt");	
+    for(int i=0; i<20000000; ++i)
+        writeStream << distr(eng) << '\n'; // generate numbers and write to file
 
-    for(int i=0; i<10; ++i)
-        std::cout << distr(eng) << '\n'; // generate numbers
+    writeStream.close();
+
+   return 0;
 }
