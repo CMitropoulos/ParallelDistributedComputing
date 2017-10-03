0d65fae74c1ee64366b937354de411e80454bdfc
root <root@node1-3.sb4.orbit-lab.org>
2017-10-03 17:54:46 -0400

    avx implementation

--- Project1/Part1/AVXIntrinsics/square_avx.cpp
+++ Project1/Part1/AVXIntrinsics/square_avx.cpp
@@ -1,6 +1,6 @@
 #include <immintrin.h>
 #include <stdio.h>
-
+#include "timing.h"
 double randomNum(int upper){
 	return (double)rand() / RAND_MAX * upper;
 }
@@ -31,14 +31,18 @@ int main() {
   
   /* Display the elements of the result vector */ 
 
+reset_and_start_timer();
  //calculate the square root
  resultVec  = _mm256_sqrt_ps(_mm256_sqrt_ps(_mm256_sqrt_ps(valuesVec)));
-  float* val = (float*)&valuesVec;
-  printf("%f %f %f %f\n", val[0], val[1], val[2], val[3]);
+
+double dt = get_elapsed_mcycles();
+
+//  float* val = (float*)&valuesVec;
+//  printf("%f %f %f %f\n", val[0], val[1], val[2], val[3]);
 	
-  float* res = (float*)&resultVec;
-  printf("%f %f %f %f\n", res[0], res[1], res[2], res[3]);
- 
+ // float* res = (float*)&resultVec;
+ // printf("%f %f %f %f\n", res[0], res[1], res[2], res[3]);
+printf("@time of AVX run:\t\t\t[%.3f] million cycles\n", dt);
    
  
   return 0;
