556eec888bc8118c3b0ebb626f35633af5629713
CMitropoulos <chris.mitropoulos@gmail.com>
2017-12-08 15:52:27 -0500

    add file reading in parallel

--- FinalProject/ParallelQuicksort/cdpSimpleQuicksort.cu
+++ FinalProject/ParallelQuicksort/cdpSimpleQuicksort.cu
@@ -2,6 +2,9 @@
 #include <cstdio>
 #include <helper_cuda.h>
 #include <helper_string.h>
+#include <string.h>
+#include <fstream>
+#include <vector>
 
 #define MAX_DEPTH       16
 #define INSERTION_SORT  32
@@ -12,16 +15,16 @@
 ////////////////////////////////////////////////////////////////////////////////
 __device__ void selection_sort( unsigned int *data, int left, int right )
 {
-  for( int i = left ; i <= right ; ++i )
+  for ( int i = left ; i <= right ; ++i )
   {
     unsigned min_val = data[i];
     int min_idx = i;
 
     // Find the smallest value in the range [left, right].
-    for( int j = i+1 ; j <= right ; ++j )
+    for ( int j = i + 1 ; j <= right ; ++j )
     {
       unsigned val_j = data[j];
-      if( val_j < min_val )
+      if ( val_j < min_val )
       {
         min_idx = j;
         min_val = val_j;
@@ -29,7 +32,7 @@ __device__ void selection_sort( unsigned int *data, int left, int right )
     }
 
     // Swap the values.
-    if( i != min_idx )
+    if ( i != min_idx )
     {
       data[min_idx] = data[i];
       data[i] = min_val;
@@ -43,39 +46,39 @@ __device__ void selection_sort( unsigned int *data, int left, int right )
 __global__ void cdp_simple_quicksort( unsigned int *data, int left, int right, int depth )
 {
   // If we're too deep or there are few elements left, we use an insertion sort...
-  if( depth >= MAX_DEPTH || right-left <= INSERTION_SORT )
+  if ( depth >= MAX_DEPTH || right - left <= INSERTION_SORT )
   {
     selection_sort( data, left, right );
     return;
   }
 
-  unsigned int *lptr = data+left;
-  unsigned int *rptr = data+right;
-  unsigned int  pivot = data[(left+right)/2];
+  unsigned int *lptr = data + left;
+  unsigned int *rptr = data + right;
+  unsigned int  pivot = data[(left + right) / 2];
 
   // Do the partitioning.
-  while(lptr <= rptr)
+  while (lptr <= rptr)
   {
     // Find the next left- and right-hand values to swap
-    unsigned int lval = *lptr; 
+    unsigned int lval = *lptr;
     unsigned int rval = *rptr;
 
     // Move the left pointer as long as the pointed element is smaller than the pivot.
-    while( lval < pivot )
+    while ( lval < pivot )
     {
       lptr++;
       lval = *lptr;
     }
 
     // Move the right pointer as long as the pointed element is larger than the pivot.
-    while( rval > pivot )
+    while ( rval > pivot )
     {
       rptr--;
       rval = *rptr;
     }
 
     // If the swap points are valid, do the swap!
-    if(lptr <= rptr)
+    if (lptr <= rptr)
     {
       *lptr++ = rval;
       *rptr-- = lval;
@@ -87,20 +90,20 @@ __global__ void cdp_simple_quicksort( unsigned int *data, int left, int right, i
   int nleft  = lptr - data;
 
   // Launch a new block to sort the left part.
-  if(left < (rptr-data)) 
+  if (left < (rptr - data))
   {
     cudaStream_t s;
     cudaStreamCreateWithFlags( &s, cudaStreamNonBlocking );
-    cdp_simple_quicksort<<< 1, 1, 0, s >>>(data, left, nright, depth+1);
+    cdp_simple_quicksort <<< 1, 1, 0, s >>>(data, left, nright, depth + 1);
     cudaStreamDestroy( s );
   }
 
   // Launch a new block to sort the right part.
-  if((lptr-data) < right) 
+  if ((lptr - data) < right)
   {
     cudaStream_t s1;
     cudaStreamCreateWithFlags( &s1, cudaStreamNonBlocking );
-    cdp_simple_quicksort<<< 1, 1, 0, s1 >>>(data, nleft, right, depth+1);
+    cdp_simple_quicksort <<< 1, 1, 0, s1 >>>(data, nleft, right, depth + 1);
     cudaStreamDestroy( s1 );
   }
 }
@@ -115,9 +118,9 @@ void run_qsort(unsigned int *data, unsigned int nitems)
 
   // Launch on device
   int left = 0;
-  int right = nitems-1;
+  int right = nitems - 1;
   std::cout << "Launching kernel on the GPU" << std::endl;
-  cdp_simple_quicksort<<< 1, 1 >>>(data, left, right, 0);
+  cdp_simple_quicksort <<< 1, 1 >>>(data, left, right, 0);
   checkCudaErrors(cudaDeviceSynchronize());
 }
 
@@ -140,54 +143,79 @@ void initialize_data(unsigned int *dst, unsigned int nitems)
 void check_results( int n, unsigned int *results_d )
 {
   unsigned int *results_h = new unsigned[n];
-  checkCudaErrors( cudaMemcpy( results_h, results_d, n*sizeof(unsigned), cudaMemcpyDeviceToHost ));
-  for( int i = 1 ; i < n ; ++i )
-    if( results_h[i-1] > results_h[i] )
+  checkCudaErrors( cudaMemcpy( results_h, results_d, n * sizeof(unsigned), cudaMemcpyDeviceToHost ));
+  for ( int i = 1 ; i < n ; ++i )
+    if ( results_h[i - 1] > results_h[i] )
     {
-      std::cout << "Invalid item[" << i-1 << "]: " << results_h[i-1] << " greater than " << results_h[i] << std::endl;
+      std::cout << "Invalid item[" << i - 1 << "]: " << results_h[i - 1] << " greater than " << results_h[i] << std::endl;
       exit(EXIT_FAILURE);
     }
   std::cout << "OK" << std::endl;
   delete[] results_h;
 }
 
+//Read File to Vector
+std::vector<int> readFiletoVector(const std::string& filename) {
+  std::ifstream source;
+  source.open(filename.c_str());
+  std::vector<int> array;
+  int number;
+
+  while (source >> number) {
+    array.push_back(number);
+  }
+  return array;
+
+}
 ////////////////////////////////////////////////////////////////////////////////
 // Main entry point.
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
   int num_items = 128;
+  std::string numbersFilename;
+
   bool verbose = false;
 
   if (checkCmdLineFlag( argc, (const char **)argv, "help" ) ||
-	  checkCmdLineFlag( argc, (const char **)argv, "h" ))
+      checkCmdLineFlag( argc, (const char **)argv, "h" ))
   {
-      std::cerr << "Usage: " << argv[0] << " num_items=<num_items>\twhere num_items is the number of items to sort" << std::endl;
-      exit(EXIT_SUCCESS);
+    std::cerr << "Usage: " << argv[0] << " num_items=<num_items>\twhere num_items is the number of items to sort" << std::endl;
+    exit(EXIT_SUCCESS);
   }
 
   if (checkCmdLineFlag( argc, (const char **)argv, "v"))
   {
-      verbose = true;
+    verbose = true;
   }
   if (checkCmdLineFlag( argc, (const char **)argv, "num_items"))
   {
-      num_items = getCmdLineArgumentInt( argc, (const char **)argv, "num_items");
-      if( num_items < 1 )
-      {
-        std::cerr << "ERROR: num_items has to be greater than 1" << std::endl;
-        exit(EXIT_FAILURE);
-      }
+    num_items = getCmdLineArgumentInt( argc, (const char **)argv, "num_items");
+    if ( num_items < 1 )
+    {
+      std::cerr << "ERROR: num_items has to be greater than 1" << std::endl;
+      exit(EXIT_FAILURE);
+    }
   }
 
+
+//CHECK FOR FILE ARGUMENT
+  if (checkCmdLineFlag( argc, (const char **)argv, "file"))
+  {
+    numbersFilename = getCmdLineArgumentInt( argc, (const char **)argv, "file");
+    std::vector<int> v = readFiletoVector(numbersFilename);
+    num_items = v.size();
+
+
+  }
   // Get device properties
   int device_count = 0, device = -1;
   checkCudaErrors( cudaGetDeviceCount( &device_count ) );
-  for( int i = 0 ; i < device_count ; ++i )
+  for ( int i = 0 ; i < device_count ; ++i )
   {
     cudaDeviceProp properties;
     checkCudaErrors( cudaGetDeviceProperties( &properties, i ) );
-    if( properties.major > 3 || ( properties.major == 3 && properties.minor >= 5 ) )
+    if ( properties.major > 3 || ( properties.major == 3 && properties.minor >= 5 ) )
     {
       device = i;
       std::cout << "Running on GPU " << i << " (" << properties.name << ")" << std::endl;
@@ -195,7 +223,7 @@ int main(int argc, char **argv)
     }
     std::cout << "GPU " << i << " (" << properties.name << ") does not support CUDA Dynamic Parallelism" << std::endl;
   }
-  if( device == -1 )
+  if ( device == -1 )
   {
     std::cerr << "cdpSimpleQuicksort requires GPU devices with compute SM 3.5 or higher.  Exiting..." << std::endl;
     exit(EXIT_SUCCESS);
@@ -208,14 +236,20 @@ int main(int argc, char **argv)
 
   // Allocate CPU memory and initialize data.
   std::cout << "Initializing data:" << std::endl;
-  h_data =(unsigned int *)malloc( num_items*sizeof(unsigned int));
-  initialize_data(h_data, num_items);
-  if( verbose )
+  h_data = (unsigned int *)malloc( num_items * sizeof(unsigned int));
+  for(int i=0;i<num_items;i++){
+    h_data[i] = v[i];
+  }
+
+
+
+  //initialize_data(h_data, num_items);
+  if ( verbose )
   {
-    for(int i=0 ; i<num_items ; i++)
+    for (int i = 0 ; i < num_items ; i++)
       std::cout << "Data [" << i << "]: " << h_data[i] << std::endl;
   }
-  
+
   // Allocate GPU memory.
   checkCudaErrors(cudaMalloc((void **)&d_data, num_items * sizeof(unsigned int)));
   checkCudaErrors(cudaMemcpy(d_data, h_data, num_items * sizeof(unsigned int), cudaMemcpyHostToDevice));
@@ -223,7 +257,7 @@ int main(int argc, char **argv)
   // Execute
   std::cout << "Running quicksort on " << num_items << " elements" << std::endl;
   run_qsort(d_data, num_items);
-  
+
   // Check result
   std::cout << "Validating results: ";
   check_results(num_items, d_data);
